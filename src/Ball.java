e7119789f6d3e7d8030fa143ed1c9a06a3cb9699
Jason Yao <jasonyao.jy@gmail.com>
2022-10-25 21:44:15 -0400

    Modified Ball class

--- src/Ball.java
+++ src/Ball.java
@@ -16,7 +16,7 @@ public class Ball {
     }
 
     public double calculateNewVelocityAngle(Rect paddle) {
-        double relativeIntersectY = (paddle.y + (paddle.height) /2.0) - (this.rect.y + (this.rect.height / 2.0));
+        double relativeIntersectY = (paddle.y + (paddle.height) / 2.0) - (this.rect.y + (this.rect.height / 2.0));
         double normalIntersectY = relativeIntersectY / (paddle.height / 2.0);
         double theta = normalIntersectY * Constants.MAX_ANGLE;
 
@@ -25,66 +25,72 @@ public class Ball {
 
     public void update(double dt) {
 
-        if (vx < 0) { // left
-            if (this.rect.x <= this.leftPaddle.x + this.leftPaddle.width && this.rect.x + this.rect.width >= this.leftPaddle.x &&
-                this.rect.y >= this.leftPaddle.y && this.rect.y <= this.leftPaddle.y + this.leftPaddle.height) {
-                double theta = calculateNewVelocityAngle(leftPaddle);
-                double newVx = Math.abs((Math.cos(theta)) * Constants.BALL_SPEED);
-                double newVy = (-Math.sin(theta)) * Constants.BALL_SPEED;
+        if (vy >= 0.0) { // down
+            if (this.rect.y + (vy * dt) + this.rect.height > Constants.SCREEN_HEIGHT - Constants.INSETS_BOTTOM) {
+                vy *= -1.0;
+            }
+        } else if (vy < 0.0) { // up
+            if (this.rect.y + (vy * dt) < Constants.TOOLBAR_HEIGHT) {
+                vy *= -1.0;
+            }
+        }
 
-                double oldSign = Math.signum(vx);
-                this.vx = newVx * (-1.0 * oldSign);
-                this.vy = newVy;
-            } else if (this.rect.x + this.rect.width < this.leftPaddle.x) {
-                int rightScore = Integer.parseInt(rightScoreText.text);
-                rightScore++;
-                rightScoreText.text = "" + rightScore;
-                this.rect.x = Constants.SCREEN_WIDTH / 2.0;
-                this.rect.y = Constants.SCREEN_HEIGHT / 2.0;
-                this.vy = 10.0;
-                this.vx = -150.0;
-                if (rightScore >= Constants.WIN_SCORE) {
-                    Main.changeState(2);
+        if (vx < 0.0) { // left
+            if (this.rect.x + (vx * dt) < leftPaddle.x + leftPaddle.width) {
+                if (this.rect.y + (vy * dt) > leftPaddle.y &&
+                        this.rect.y + (vy * dt) + this.rect.height < leftPaddle.y + leftPaddle.height) {
+                    double theta = calculateNewVelocityAngle(leftPaddle);
+                    double newVx = Math.abs((Math.cos(theta)) * Constants.BALL_SPEED);
+                    double newVy = (-Math.sin(theta)) * Constants.BALL_SPEED;
+
+                    double oldSign = Math.signum(vx);
+                    this.vx = newVx * (-1.0 * oldSign);
+                    this.vy = newVy;
                 }
             }
-        } else if (vx > 0)  { // right
-            if (this.rect.x + this.rect.width >= this.rightPaddle.x && this.rect.x <= this.rightPaddle.x + this.rightPaddle.width &&
-                    this.rect.y >= this.rightPaddle.y && this.rect.y <= this.rightPaddle.y + this.rightPaddle.height) {
-                double theta = calculateNewVelocityAngle(rightPaddle);
-                double newVx = Math.abs((Math.cos(theta)) * Constants.BALL_SPEED);
-                double newVy = (-Math.sin(theta)) * Constants.BALL_SPEED;
+        } else if (vx >= 0.0) { // right
+            if (this.rect.x + (vx * dt) + rect.width > rightPaddle.x) {
+                if (this.rect.y + (vy * dt) > rightPaddle.y &&
+                        this.rect.y + (vy * dt) + this.rect.height < rightPaddle.y + rightPaddle.height) {
+                    double theta = calculateNewVelocityAngle(rightPaddle);
+                    double newVx = Math.abs((Math.cos(theta)) * Constants.BALL_SPEED);
+                    double newVy = (-Math.sin(theta)) * Constants.BALL_SPEED;
 
-                double oldSign = Math.signum(vx);
-                this.vx = newVx * (-1.0 * oldSign);
-                this.vy = newVy;
-            } else if (this.rect.x + this.rect.width > this.rightPaddle.x + this.rightPaddle.width) {
-                int leftScore = Integer.parseInt(leftScoreText.text);
-                leftScore++;
-                leftScoreText.text = "" + leftScore;
-                this.rect.x = Constants.SCREEN_WIDTH / 2.0;
-                this.rect.y = Constants.SCREEN_HEIGHT / 2.0;
-                this.vy = 10.0;
-                this.vx = -150.0;
-                if (leftScore >= Constants.WIN_SCORE) {
-                    Main.changeState(2);
+                    double oldSign = Math.signum(vx);
+                    this.vx = newVx * (-1.0 * oldSign);
+                    this.vy = newVy;
                 }
             }
         }
 
-        if (vy > 0) { // down
-            if (this.rect.y + this.rect.height > Constants.SCREEN_HEIGHT) {
-                this.vy *= -1;
-            }
-        } else if (vy < 0) { // up
-            if (this.rect.y < Constants.TOOLBAR_HEIGHT) {
-                this.vy *= -1;
-            }
-        }
 
         // position = position + velocity
         // velocity = velocity + acceleration
         this.rect.x += vx * dt;
         this.rect.y += vy * dt;
 
+        if (this.rect.x + this.rect.width < this.leftPaddle.x) {
+            int rightScore = Integer.parseInt(rightScoreText.text);
+            rightScore++;
+            rightScoreText.text = "" + rightScore;
+            this.rect.x = Constants.SCREEN_WIDTH / 2.0;
+            this.rect.y = Constants.SCREEN_HEIGHT / 2.0;
+            this.vy = 10.0;
+            this.vx = -150.0;
+            if (rightScore >= Constants.WIN_SCORE) {
+                Main.changeState(2);
+            }
+        } else if (this.rect.x + this.rect.width > this.rightPaddle.x + this.rightPaddle.width) {
+            int leftScore = Integer.parseInt(leftScoreText.text);
+            leftScore++;
+            leftScoreText.text = "" + leftScore;
+            this.rect.x = Constants.SCREEN_WIDTH / 2.0;
+            this.rect.y = Constants.SCREEN_HEIGHT / 2.0;
+            this.vy = 10.0;
+            this.vx = -150.0;
+            if (leftScore >= Constants.WIN_SCORE) {
+                Main.changeState(2);
+            }
+        }
     }
 }
